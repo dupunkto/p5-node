569e5944960e1427e0840a088e6daf61811df223
Robin Boers <robindev2019@outlook.com>
2024-03-05 16:53:45 +0100

    Add missing instance variables

--- src/core/main.js
+++ src/core/main.js
@@ -11,6 +11,12 @@ export default class p5 {
   constructor() {
     this._lcg_random_state = null;
     this._gaussian_previous = false;
+    this._colorMode = constants.RGB;
+    this._colorMaxes = {
+      rgb: [255, 255, 255, 255],
+      hsb: [360, 100, 100, 1],
+      hsl: [360, 100, 100, 1],
+    };
   }
 }
 
